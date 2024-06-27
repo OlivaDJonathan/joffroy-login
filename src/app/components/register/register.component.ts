9c9417754749cd655ef5d7846d5acd84f59144b2
Jonathan Oliva <jonathan.oliva.2077@gmail.com>
2024-06-26 20:43:35 -0600

    Added Register Component and minor QOL for Forms

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [NgIf, ReactiveFormsModule, RouterLink],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  StrongPasswordRegx: RegExp =/^(?=[^A-Z]*[A-Z])(?=[^a-z]*[a-z])(?=\D*\d).{8,}$/;
 
+  userForm: any;
+
+  active : boolean = true;
+
+  constructor(private formBuilder: FormBuilder) {}
+  ngOnInit(): void {
+    this.userForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.pattern(this.StrongPasswordRegx)]]
+    });
+  }
+
+  register() {
+    //TODO Login submition
+    if (this.userForm?.valid) {
+      console.log('Form data:', this.userForm.value);
+    }
+  }
 }
