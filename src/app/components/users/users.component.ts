ce1a171aaca79b63b629606cdba25929fa45d0bc
Jonathan Oliva <jonathan.oliva.2077@gmail.com>
2024-07-01 03:02:54 -0600

    Added User list and minor QOL

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, afterNextRender } from '@angular/core';
+import { UsersService } from '../../services/users-service.service';
+import { User } from '../../models/user';
+import { AsyncPipe } from '@angular/common';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './users.component.html',
   styleUrl: './users.component.scss'
 })
-export class UsersComponent {
+export class UsersComponent{
+
+  users$!: Observable<User[]>;
+
+  constructor(
+    private _userService: UsersService
+  ) {
+    afterNextRender(() => {
+      this.users$ = this.getUsers();
+    })
+  }
+
+  getUsers(): Observable<User[]>{
+    return this._userService.getUsers();
+  }
 
 }
