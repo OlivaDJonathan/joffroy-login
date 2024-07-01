186e4d7cd809df1e156f2f90768c139a876291cd
Jonathan Oliva <jonathan.oliva.2077@gmail.com>
2024-07-01 16:49:25 -0600

    User Form page

--- src/app/components/user-form/user-form.component.ts
+++ src/app/components/user-form/user-form.component.ts
@@ -1,12 +1,86 @@
-import { Component } from '@angular/core';
+import { UsersService } from './../../services/users-service.service';
+import { Observable } from 'rxjs';
+import { Component, Input, OnChanges, OnInit, SimpleChanges, afterNextRender } from '@angular/core';
+import { User } from '../../models/user';
+import { AsyncPipe, NgIf } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-form',
   standalone: true,
-  imports: [],
+  imports: [NgIf,AsyncPipe, ReactiveFormsModule],
   templateUrl: './user-form.component.html',
   styleUrl: './user-form.component.scss'
 })
-export class UserFormComponent {
+export class UserFormComponent implements OnChanges{
+  @Input() user$!: Observable<User> | undefined;
+  @Input() newUser!: boolean;
+  @Input() action!: string;
+  @Input() currentId!: string;
 
+  userForm: any;
+
+  PhoneNumber: RegExp =/^(\+\d{1,2}\s?)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/;
+
+  constructor (
+    private formBuilder: FormBuilder, 
+    private _userService: UsersService
+  ) {}
+  ngOnChanges(changes: SimpleChanges): void {
+    this.populateForm();
+  }
+
+  populateForm(): void {
+    if (this.user$ != undefined && !this.newUser){
+      this.user$?.subscribe(res => {
+        const newUser: User = {
+          id: res.id,
+          firstName: res.firstName,
+          lastName: res.lastName,
+          email: res.email,
+          phone: res.phone,
+          role: res.role
+        }
+        
+        this.userForm = this.formBuilder.group(newUser);
+      })
+    } else {
+      this.userForm = this.formBuilder.group({
+        firstName: ['', Validators.required],
+        lastName: ['', Validators.required],
+        phone: ['', [Validators.required, Validators.pattern(this.PhoneNumber)]],
+        email: ['', [Validators.required, Validators.email]]
+      });
+    }
+  }
+
+  save(): void {
+    if (this.newUser){
+      var newUser: User = {
+        firstName: this.userForm.value.firstName,
+        lastName: this.userForm.value.lastName,
+        email: this.userForm.value.email,
+        phone: this.userForm.value.phone,
+        role: 'user'
+      }
+
+      this._userService.createUser(newUser).subscribe();
+    } else {
+
+      var existingUser: User = {
+        id: this.currentId,
+        firstName: this.userForm.value.firstName,
+        lastName: this.userForm.value.lastName,
+        email: this.userForm.value.email,
+        phone: this.userForm.value.phone,
+        role: 'user'
+      }
+
+      this._userService.updateUser(this.currentId, existingUser).subscribe();
+    }
+  }
+
+  delete(): void{
+    this._userService.deleteUser(this.currentId).subscribe();
+  }
 }
--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -3,17 +3,22 @@ import { UsersService } from '../../services/users-service.service';
 import { User } from '../../models/user';
 import { AsyncPipe } from '@angular/common';
 import { Observable } from 'rxjs';
+import { UserFormComponent } from "../user-form/user-form.component";
 
 @Component({
-  selector: 'app-users',
-  standalone: true,
-  imports: [AsyncPipe],
-  templateUrl: './users.component.html',
-  styleUrl: './users.component.scss'
+    selector: 'app-users',
+    standalone: true,
+    templateUrl: './users.component.html',
+    styleUrl: './users.component.scss',
+    imports: [AsyncPipe, UserFormComponent]
 })
 export class UsersComponent{
 
   users$!: Observable<User[]>;
+  user$!: Observable<User> | undefined;
+  newUser: boolean = false;
+  action: string = '';
+  currentId: string = '';
 
   constructor(
     private _userService: UsersService
@@ -25,6 +30,20 @@ export class UsersComponent{
 
   getUsers(): Observable<User[]>{
     return this._userService.getUsers();
+  }
+
+  getUser(id: string) {
+    this.user$ = this._userService.getUser(id);
+    this.newUser = false;
+    this.action = 'Update';
+    this.currentId = id;
+  }
+
+  createNewUser(): void {
+    this.newUser = true;
+    this.user$ = undefined;
+    this.action = 'Create';
+    this.currentId = '';
   }
 
 }
